ce53c5e05ab77b9c5e6f0db7f1be0713875a2f61
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-18 13:24:34 +0200

    Fixed missing space in hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ export const Hero = () => {
 	return (
 		<section className="py-12 mb-4">
 			<h1 className="text-5xl mt-4 font-bold text-white">
-				Find your next game with
+				Find your next game with{" "}
 				<span className="text-violet-500 font-bold">Play</span>
 				<span className="text-white">Mor</span>
 			</h1>
