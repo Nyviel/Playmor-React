b5d78a752c859648ae89e1b2713ba6e03d4e1e6e
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-18 13:24:03 +0200

    Added fetching by dates to gameService

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -1,9 +1,54 @@
+import { IGame } from "@/interfaces/game";
 import { API } from "../utilities/constants";
 
-export const fetchHotPicks = async (): Promise<Response> => {
-	return fetch(`${API}/games`);
+export const fetchHotPicks = async (): Promise<IGame[]> => {
+	const response = await fetch(`${API}/games`);
+
+	if (response.ok) {
+		return response.json();
+	} else {
+		throw new Error(
+			"Failed to fetch hot picks" + response.status + response.statusText
+		);
+	}
 };
 
-export const fetchGameById = async (gameId: number): Promise<Response> => {
-	return fetch(`${API}/games/${gameId}`);
+export const fetchGameById = async (id: number): Promise<IGame> => {
+	const response = await fetch(`${API}/games/${id}`);
+
+	if (response.ok) {
+		return response.json();
+	} else {
+		throw new Error(
+			"Failed to fetch game" + response.status + response.statusText
+		);
+	}
+};
+
+export const fetchGamesByAddedDate = async (
+	sortOrder = "desc"
+): Promise<IGame[]> => {
+	const response = await fetch(`${API}/games/added?sort=${sortOrder}`);
+
+	if (response.ok) {
+		return response.json();
+	} else {
+		throw new Error(
+			"Failed to fetch games" + response.status + response.statusText
+		);
+	}
+};
+
+export const fetchGamesByReleasedDate = async (
+	sortOrder = "desc"
+): Promise<IGame[]> => {
+	const response = await fetch(`${API}/games/released?sort=${sortOrder}`);
+
+	if (response.ok) {
+		return response.json();
+	} else {
+		throw new Error(
+			"Failed to fetch games" + response.status + response.statusText
+		);
+	}
 };
