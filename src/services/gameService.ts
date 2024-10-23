9522309b8505165d86d7d235e99fccff379bf60d
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-23 12:41:55 +0200

    Added Explore component and the game service paginate method

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -1,5 +1,6 @@
 import { IGame } from "@/interfaces/game";
 import { API } from "../utilities/constants";
+import { IGamePagination } from "@/interfaces/gamePagination";
 
 export const fetchHotPicks = async (): Promise<IGame[]> => {
 	const response = await fetch(`${API}/games`);
@@ -49,6 +50,27 @@ export const fetchGamesByReleasedDate = async (
 	} else {
 		throw new Error(
 			"Failed to fetch games" + response.status + response.statusText
+		);
+	}
+};
+
+export const fetchGamesPaginated = async (
+	pageNumber: number,
+	sortBy: string,
+	fromDate: string,
+	toDate: string
+): Promise<IGamePagination> => {
+	const response = await fetch(
+		`${API}/games/paginated?pageNumber=${pageNumber}&pageSize=9&sortBy=${sortBy}&fromDate=${fromDate}&toDate=${toDate}`
+	);
+
+	if (response.ok) {
+		return response.json();
+	} else {
+		throw new Error(
+			"Failed to fetch paginated games" +
+				response.status +
+				response.statusText
 		);
 	}
 };
