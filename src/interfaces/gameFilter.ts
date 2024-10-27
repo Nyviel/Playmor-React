7c0a49777f22d643955c376f327257e76b173041
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-10-27 14:01:49 +0100

    Added new interfaces

---
+++ src/interfaces/gameFilter.ts
@@ -0,0 +1,10 @@
+export interface GameFilter {
+	fromDate: string;
+	toDate: string;
+	keyword: string;
+	modes: string[];
+	genres: string[];
+	platforms: string[];
+	developers: string[];
+	publishers: string[];
+}
