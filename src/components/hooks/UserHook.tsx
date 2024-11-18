1e6bfbadd837734dcee66565a2c7ce8daacd78c3
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-11-18 21:37:17 +0100

    Delete old userHook files

--- src/components/hooks/UserHook.tsx
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { IUserContextHook, UserContext } from "../providers/UserProvider";
-
-export const useUser = (): IUserContextHook => useContext(UserContext);
